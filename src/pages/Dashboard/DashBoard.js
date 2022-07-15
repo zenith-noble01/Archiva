2ee2c7b70b5df7ec1b1e78e882912d3035f5c5d6
zenith-noble01 <webdev686@gmail.com>
2022-07-15 15:48:38 +0100

    the mile

--- src/pages/Dashboard/DashBoard.js
+++ src/pages/Dashboard/DashBoard.js
@@ -2,10 +2,14 @@ import React from "react";
 import "./Dashboard.scss";
 import { Sidebar } from "../../Components";
 import { Outlet } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 const DashBoard = () => {
   return (
     <div className="app__dashboad">
+      <Helmet>
+        <title>Archiva | Dashboard</title>
+      </Helmet>
       <Sidebar />
       <div className="dashBoard__wrapper">
         <Outlet />
--- src/pages/Doctors/Doctors.js
+++ src/pages/Doctors/Doctors.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./Doctors.scss";
+import { Helmet } from "react-helmet";
 
 const Doctors = () => {
-  return <div>Doctors</div>;
+  return (
+    <div>
+      <Helmet>
+        <title>Archiva | Doctors</title>
+      </Helmet>
+      <div className="outlet__doctors">
+        <div className="doctor__header">
+          <h3>Doctors</h3>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Doctors;
